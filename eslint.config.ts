87bc4d249a3732246cc4932b4b8f8582b5483b08
yangxu52 <yyswj10@163.com>
2025-03-02 17:02:39 +0800

    refactor: migrate ESLint 9 & re-config
    - migrate ESLint v9
    - migrate ESLint flat config
    - migrate prettier v3
    - update .prettier.json

---
+++ eslint.config.ts
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['**/dist', '**/node_modules'],
+  },
+  {
+    files: ['packages/**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'no-console': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]

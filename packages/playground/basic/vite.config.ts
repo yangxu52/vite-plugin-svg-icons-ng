390561baf1cbbd81630ab219bb284b1b0c8ab6ef
yangxu52 <yyswj10@163.com>
2025-03-02 16:41:35 +0800

    chore: remove useless dependencies
    - workspace
      - @types/html-minifier-terser
      - cross-env
      - rimraf
      - tsup
    - playground
      - etag
      - fs-extra
      - svgo
      - @vitejs/plugin-vue-jsx
      - @vue/compiler-sfc
      - cross-env

--- packages/playground/basic/vite.config.ts
+++ packages/playground/basic/vite.config.ts
@@ -1,6 +1,5 @@
 import { UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import jsx from '@vitejs/plugin-vue-jsx'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
@@ -8,7 +7,6 @@ export default (): UserConfigExport => {
   return {
     plugins: [
       vue(),
-      jsx(),
       createSvgIconsPlugin({
         iconDirs: [path.resolve(process.cwd(), 'src/icons')],
         // icon symbolId
